31f94b1ac984c6042b956d698186d271038b7d6d
egregors <root@egregors.com>
2020-09-28 19:13:32 +1000

    feat: status field for Inference model

--- moon_vision_challenge/classification/models.py
+++ moon_vision_challenge/classification/models.py
@@ -5,6 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class Inference(models.Model):
     """ TODO: add docstring """
 
+    # TODO: extract it into module with keras
     class NETWORKS(models.TextChoices):
         """ Pre-trained keras models """
         DenseNet121 = "DenseNet121", "DenseNet121"
@@ -26,11 +27,20 @@ class Inference(models.Model):
         VGG19 = "VGG19", "VGG19"
         Xception = "Xception", "Xception"
 
-    model_type = models.CharField(_("the type of pretrained model"), max_length=64, choices=NETWORKS.choices)
+    class STATUS(models.TextChoices):
+        """ Status of Inference """
+        PENDING = "pending", "waits for processing"
+        PROCESSING = "processing", "processing in progress"
+        DONE = "done", "processing is done"
+
+    model_type = models.CharField(_("the type of pretrained model"), max_length=64, choices=NETWORKS.choices,
+                                  default=NETWORKS.VGG16)
     # TODO:
     #  - [ ] define a path to store?
     #  - [ ] delete images after processing
     image = models.ImageField(_("image"), help_text=_("image to classification"))
+
+    status = models.CharField(_("status"), max_length=16, choices=STATUS.choices, default=STATUS.PENDING)
     result_label = models.CharField(_("label"), max_length=255,
                                     help_text=_("label the image was classified with"))
 

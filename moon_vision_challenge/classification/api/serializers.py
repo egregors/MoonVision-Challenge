ba10ffdd6b73fb2f02869524dbd042ed58ed5ab0
egregors <root@egregors.com>
2020-09-28 19:14:40 +1000

    dev: serializer and for naive inference (without classification)

--- moon_vision_challenge/classification/api/serializers.py
+++ moon_vision_challenge/classification/api/serializers.py
@@ -6,6 +6,13 @@ from moon_vision_challenge.classification.models import Inference
 User = get_user_model()
 
 
+# noinspection PyAbstractClass
+class NaiveInferenceSerializer(serializers.Serializer):
+    model_type = serializers.ChoiceField(choices=Inference.NETWORKS.choices, required=True)
+    image = serializers.FileField(required=True)
+    result_label = serializers.CharField(allow_blank=True, read_only=True)
+
+
 class InferenceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Inference
--- moon_vision_challenge/classification/api/views.py
+++ moon_vision_challenge/classification/api/views.py
@@ -1,19 +1,45 @@
 from django.contrib.auth import get_user_model
+from rest_framework import mixins, status
 from rest_framework.permissions import AllowAny
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
 
-from .serializers import InferenceSerializer
-from ..models import Inference
+from .serializers import NaiveInferenceSerializer
 
 User = get_user_model()
 
 
-class NaiveInferenceViewSet(ModelViewSet):
+# TODO: extract it to module with keras
+def get_label_for_img(model_type, image):
+    raise NotImplemented()
+
+
+class NaiveInferenceViewSet(mixins.CreateModelMixin,
+                            GenericViewSet):
     """ TODO: add doc """
-    serializer_class = InferenceSerializer
-    queryset = Inference.objects.all()
+    serializer_class = NaiveInferenceSerializer
+    # TODO: change perms for prod
     permission_classes = [AllowAny]
 
     def create(self, request, *args, **kwargs):
-        # TODO: straight call of keras application
-        return super().create(request, *args, **kwargs)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        # nothing to save
+        # self.perform_create(serializer)
+
+        data = serializer.validated_data
+        mt = data["model_type"]
+        img = data["image"]
+
+        # TODO: make classification here
+        result_label = get_label_for_img(model_type=mt, image=img)
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            {
+                'result_label': result_label
+            },
+            status=status.HTTP_200_OK,
+            headers=headers
+        )

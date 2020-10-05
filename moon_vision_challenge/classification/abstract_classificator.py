6e837cf07f804830f3fc1a6ac7dbee80c18a7d63
egregors <root@egregors.com>
2020-10-05 16:47:02 +1000

    dev: base dispatcher and classificator

--- moon_vision_challenge/classification/abstract_classificator.py
+++ moon_vision_challenge/classification/abstract_classificator.py
@@ -1,21 +1,35 @@
 import logging
 from abc import ABC, abstractmethod
+from typing import Any
+
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 logger = logging.getLogger(__name__)
 
 
 class Classificator(ABC):
     """ The base classificator meta class. All implementations should be inheritance from this one.
-
-    TODO: add docs for abs methods
+    Required implementation `preprocess_input` and `decode_predictions` methods.
     """
 
     model_type: str
 
     @abstractmethod
-    def preprocess_input(self):
+    def preprocess_input(self, image: InMemoryUploadedFile) -> Any:
+        """
+        Convert Image to proper type according ML-model signature.
+
+        :param image: InMemoryUploadedFile object from request
+        :return: Object which expected by ML-model
+        """
         raise NotImplementedError()
 
     @abstractmethod
-    def decode_predictions(self):
+    def decode_predictions(self) -> Any:
+        """
+        Decode predictions from a model, and convert it to useful type.
+
+        :rtype: Any (consumer choice)
+        :return: Representation of ML-model predictions
+        """
         raise NotImplementedError()
--- moon_vision_challenge/classification/dispatchers.py
+++ moon_vision_challenge/classification/dispatchers.py
@@ -16,7 +16,7 @@ class BaseClassificatorDispatcher(ABC):
 
 
 class DefaultClassificatorDispatcher(BaseClassificatorDispatcher):
-    """ Default Dispatcher for Classificators """
+    """ Default Dispatcher for Classificators. """
     _classificators: Dict[str, Type[Classificator]]
 
     def __str__(self) -> str:
@@ -25,11 +25,31 @@ class DefaultClassificatorDispatcher(BaseClassificatorDispatcher):
     def __getitem__(self, model_type: str) -> Type[Classificator]:
         return self._classificators[model_type]
 
-    def _classificator_is_valid(self, classificator: Type[Classificator]):
-        # TODO: add check of imp all abs methods
+    def has_classificator(self, model_type: str) -> bool:
+        """ Check if Dispatcher has handler for particular model type classificator. """
+        return model_type in self._classificators
+
+    def _classificator_is_valid(self, classificator: Type[Classificator]) -> bool:
+        if not classificator.model_type:
+            raise ValueError('`model_type` attribute is required for all Classificators')
+
+        if self.has_classificator(classificator.model_type):
+            raise ValueError('`{0}` model type already registered'.format(classificator.model_type))
+
+        if getattr(classificator.preprocess_input, '__isabstractmethod__', False):
+            raise ValueError('`preprocess_input` method is required')
+
+        if getattr(classificator.decode_predictions, '__isabstractmethod__', False):
+            raise ValueError('`decode_predictions` method is required')
+
         return True
 
     def register(self, classificator: Type[Classificator]):
         if self._classificator_is_valid(classificator):
             logger.info('registering action `%s`', classificator.model_type)
             self._classificators[classificator.model_type] = classificator
+
+
+dispatcher = DefaultClassificatorDispatcher()
+
+__all__ = ['dispatcher']

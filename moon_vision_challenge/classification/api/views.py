2c56a9d4ae7ac1c501daa8a19bc2aa1c4cf7e31f
egregors <root@egregors.com>
2020-10-05 20:43:16 +1000

    feat: naive view

--- moon_vision_challenge/classification/api/views.py
+++ moon_vision_challenge/classification/api/views.py
@@ -1,45 +1,39 @@
-from django.contrib.auth import get_user_model
+import logging
+
 from rest_framework import mixins, status
+from rest_framework.exceptions import APIException
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from .serializers import NaiveInferenceSerializer
+from ..dispatchers import dispatcher as classificators
 
-User = get_user_model()
-
-
-# TODO: extract it to module with keras
-def get_label_for_img(model_type, image):
-    raise NotImplemented()
+logger = logging.getLogger(__name__)
 
 
+# TODO: add throttling
 class NaiveInferenceViewSet(mixins.CreateModelMixin,
                             GenericViewSet):
-    """ TODO: add doc """
+    """ Naive way to make quick prediction. This is a synchronous view. """
+
     serializer_class = NaiveInferenceSerializer
-    # TODO: change perms for prod
     permission_classes = [AllowAny]
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        # nothing to save
-        # self.perform_create(serializer)
+        model_type = serializer.validated_data['model_type']
+        image = serializer.validated_data['image']
 
-        data = serializer.validated_data
-        mt = data["model_type"]
-        img = data["image"]
-
-        # TODO: make classification here
-        result_label = get_label_for_img(model_type=mt, image=img)
+        try:
+            label = classificators[model_type](image=image).predict()
+        except Exception as err:
+            logger.error("can't make prediction: %s", err)
+            raise APIException("can't make prediction: %s" % err) from err
 
         headers = self.get_success_headers(serializer.data)
         return Response(
-            {
-                'result_label': result_label
-            },
-            status=status.HTTP_200_OK,
-            headers=headers
+            {'label': label}, status=status.HTTP_200_OK, headers=headers
         )

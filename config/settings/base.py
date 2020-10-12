68da6ea12fdf19b7091074946abf052ffebb51a6
egregors <root@egregors.com>
2020-10-12 22:29:28 +1000

    dev: add THROTTLE for v0 and v1 views

--- config/settings/base.py
+++ config/settings/base.py
@@ -275,6 +275,9 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '1/sec',
+    }
 }
 
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
